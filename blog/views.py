a40975d2a9f1b0f718abe5516a6589cfd7952698
medicalcobi <cho140217@naver.com>
2021-05-10 02:14:40 +0900

    f로 상세페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -8,8 +8,21 @@ def index(request):
 
     return render(
         request,
-        'blog/index.html',
+        'blog/post_list.html',
         {
             'posts': posts,
         }
     )
+
+def single_post_page(request, pk):
+    post = Post.objects.get(pk=pk)
+
+    return render(
+        request,
+        'blog/single_post_page.html',
+        {
+            'post': post,
+        }
+    )
+
+
